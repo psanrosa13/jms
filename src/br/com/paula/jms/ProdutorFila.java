93887dca4a5353d5f47069a64ca949e545ee0225
psanrosa13 <psanrosa13@gmmail.com>
2019-02-18 21:38:20 -0300

    arquivos testes de jms

---
+++ src/br/com/paula/jms/ProdutorFila.java
@@ -0,0 +1,33 @@
+package br.com.paula.jms;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class ProdutorFila {
+
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		Connection connection = factory.createConnection(); 
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination fila = (Destination) context.lookup("fila");
+		
+		MessageProducer producer = session.createProducer(fila);
+		
+		Message message = session.createTextMessage("Mensagem 1");
+		producer.send(message);
+				
+		session.close();
+		connection.close();
+		context.close();
+	}
+}
---
+++ src/br/com/paula/jms/ProdutorTopico.java
@@ -0,0 +1,33 @@
+package br.com.paula.jms;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class ProdutorTopico {
+
+	
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		Connection connection = factory.createConnection(); 
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination topico = (Destination) context.lookup("topico");
+		MessageProducer producer = session.createProducer(topico);
+		
+		Message message = session.createTextMessage("Mensagem 1");
+        producer.send(message);
+		
+		session.close();
+		connection.close();
+		context.close();
+	}
+}
